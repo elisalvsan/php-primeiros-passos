9b0803feeac5a7a023600db5e467f3aaecdbac82
Elisa Andrade <elisalvsan@gmail.com>
2023-05-21 22:40:57 -0300

    Repetições - FOR e WHILE

---
+++ repeticao.php
@@ -0,0 +1,19 @@
+<?php
+
+//desafio - exiba uma lista de 1 a 15;
+
+/* FOR
+for ($i = 1; $i <= 15; $i++){
+    echo "#$i" . PHP_EOL;
+}
+*/
+
+
+/* WHILE
+$i = 1;
+
+while ($i <= 15){
+    echo "#$i" . PHP_EOL;
+    $i++;
+}
+*/
