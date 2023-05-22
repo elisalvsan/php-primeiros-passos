d8375fbb641c55b64f92129df768a25d31924314
Elisa Andrade <elisalvsan@gmail.com>
2023-05-21 23:02:55 -0300

    Desafio - Numero Impares

---
+++ desafios/NumerosImpares.php
@@ -0,0 +1,16 @@
+<?php
+
+for ($i = 0; $i <=100; $i++){
+    if($i % 2 != 0){
+        echo "$i" . PHP_EOL;
+    }
+}
+
+/*
+for ($i = 0; $i <=100; $i++){
+    if($i % 2 != 0){
+        echo "Numero Impar = $i" . PHP_EOL;
+    }else{
+        echo "Numero Par = $i" . PHP_EOL;
+    }
+}*/
